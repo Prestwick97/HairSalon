8c8ad9a14b8c261ca70d35b8edb3dc37f27076d3
Prestwick97 <prestwick97@gmail.com>
2020-03-20 16:55:07 -0700

    fix typo in Client.cs

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -2,7 +2,7 @@ namespace HairSalon.Models
 {
   public class Client
   {
-    public int ClientID {get; set;}
+    public int ClientId {get; set;}
     public string Description {get; set;}
     public int StylistId {get; set;}
     public virtual Stylist stylist {get; set;}
